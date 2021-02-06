dc8528839cdc9f6399cdb9f3aefc12fd1bcd74f0
Caique Silveira <38363493+CaiqueSilveira@users.noreply.github.com>
2021-02-06 17:25:22 -0300

    Update no arquivo coon.php

--- sys/view/relatorio/conn.php
+++ sys/view/relatorio/conn.php
@@ -1,13 +1,45 @@
 <?php
-
 function abrirBanco(){
-    $conexao = new mysqli("localhost", "root", "", "db_sgd");
+    $conexao = new mysqli("sql302.epizy.com", "epiz_25627353", "OzKcndk34w0", "epiz_25627353_db_sgd");
     return $conexao;
 }
 
 function selectAllPendente(){
     $banco = abrirBanco();
-    $sql = "SELECT * FROM sgd_divida WHERE status ='P'";
+    $sql = " SELECT * FROM sgd_cliente INNER JOIN sgd_divida ON sgd_cliente.id = sgd_divida.cliente AND sgd_divida.status='N'";
+    $resultado = $banco->query($sql);
+    $banco->close();
+    while ($row = mysqli_fetch_array($resultado)) {
+        $grupo[] = $row;
+    }
+    return $grupo;
+}
+
+function selectAllPagas(){
+    $banco = abrirBanco();
+    $sql = "SELECT * FROM sgd_cliente INNER JOIN sgd_divida ON sgd_cliente.id = sgd_divida.cliente AND sgd_divida.status='P'";
+    $resultado = $banco->query($sql);
+    $banco->close();
+    while ($row = mysqli_fetch_array($resultado)) {
+        $grupo[] = $row;
+    }
+    return $grupo;
+}
+
+function selectTotalP(){
+    $banco = abrirBanco();
+    $sql = "SELECT valor, status, SUM(valor) AS valor FROM sgd_divida WHERE status='P'";
+    $resultado = $banco->query($sql);
+    $banco->close();
+    while ($row = mysqli_fetch_array($resultado)) {
+        $grupo[] = $row;
+    }
+    return $grupo;
+}
+
+function selectTotalN(){
+    $banco = abrirBanco();
+    $sql = "SELECT valor, status, SUM(valor) AS valor FROM sgd_divida WHERE status='N'";
     $resultado = $banco->query($sql);
     $banco->close();
     while ($row = mysqli_fetch_array($resultado)) {
